3da8338fd155fd76f168ebc409d54bf23273326d
Ali <biz.alihabibi@gmail.com>
2025-02-02 00:01:15 +0330

    Selectable Time Zones

--- app/src/main/java/com/sample/ali/timeapi/MainActivity.kt
+++ app/src/main/java/com/sample/ali/timeapi/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sample.ali.timeapi
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.sample.ali.timeapi.api.ApiRepository
 import com.sample.ali.timeapi.api.ApiRespond
@@ -10,10 +12,30 @@ import com.sample.ali.timeapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var selectedTimeZone = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val timeZones = arrayListOf(
+            "America/Buenos_Aires",
+            "America/Los_Angeles",
+            "America/Mexico_City",
+            "Asia/Dubai",
+            "Asia/Gaza",
+            "Asia/Seoul",
+            "Australia/NSW",
+            "Europe/Berlin",
+            "Europe/Madrid",
+            "Pacific/Apia",
+            "Etc/GMT",
+        )
+        binding.txtSelectedTimeZone.text = timeZones[0]
+        binding.timeZonesSpinner.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            timeZones
+        )
         val getTimeByTimeZoneRespond = object : ApiRespond {
             override fun onRespond(respond: MainModel) {
                 binding.txtTime.text = buildString {
@@ -29,7 +51,29 @@ class MainActivity : AppCompatActivity() {
                 binding.progressBar.visibility = View.INVISIBLE
             }
         }
-        val selectedTimeZone = "Asia/Singapore"
+        selectedTimeZone = timeZones[0]
         ApiRepository.instance.getTimeByTimeZone(selectedTimeZone, getTimeByTimeZoneRespond)
+        binding.timeZonesSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    adapterView: AdapterView<*>?,
+                    spinner: View?,
+                    position: Int,
+                    rowId: Long
+                ) {
+                    binding.progressBar.visibility = View.VISIBLE
+                    selectedTimeZone = timeZones[position]
+                    binding.txtSelectedTimeZone.text = timeZones[position]
+                    ApiRepository.instance.getTimeByTimeZone(
+                        selectedTimeZone,
+                        getTimeByTimeZoneRespond
+                    )
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    // Nothing
+                }
+
+            }
     }
 }
